2c0c98b546eb9b24367661831fe7208cbc07a52e
osanchez <osanchez@umass.edu>
2018-02-14 04:05:26 -0500

    completed encode, decode

--- main.py
+++ main.py
@@ -23,8 +23,6 @@ def encode(codepoint):
         byte_outer_right = (0b10 << 6) | (0b000000000000000111111 & codepoint)
         print([byte_outer_left, byte_inner_left, byte_inner_right, byte_outer_right])
         return bytes([byte_outer_left, byte_inner_left, byte_inner_right, byte_outer_right])
-    else:
-        print("outside of boundary...")
 
 
 def decode(bytes_object):
@@ -36,22 +34,10 @@ def decode(bytes_object):
         return (0b00001111 & bytes_object[0] << 12) | (0b00111111 & bytes_object[1]) << 6 | (0b00111111 & bytes_object[2])
     elif len(bytes_object) == 4:
         return (0b00000111 & bytes_object[0] << 18) | (0b00111111 & bytes_object[1]) << 12 | (0b00111111 & bytes_object[2]) << 6 | (0b00111111 & bytes_object[3])
-    else:
-        print("number of bytes out of boundary...")
 
 
 def main():
-    print(encode(122))
-    print(decode(b'z'))
-    print()
-    print(encode(168))
-    print(decode(b'\xc2\xa8'))
-    print()
-    print(encode(2055))
-    print(decode(b'\xe0\xa0\x87'))
-    print()
-    print(encode(65537))
-    print(decode(b'\xf0\x90\x80\x81'))
+    pass
 
 
 if __name__ == "__main__":
