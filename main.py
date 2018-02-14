39d4bd187136973375e0f4739b6e366c48324e94
Omar S <osanchez@umass.edu>
2018-02-14 01:43:04 -0500

    initial commit

--- main.py
+++ main.py
@@ -2,9 +2,25 @@
 
 def encode(codepoint):
     if codepoint < 128:
-        x = codepoint % 16
-        if x == 0:
-        return codepoint
+        return bytes([codepoint])
+
+    elif 128 <= codepoint <= 2047:
+        byte_left = 0b11000000 | (0b00000000000 | codepoint) >> 6
+        print(bin(byte_left))
+        byte_right = 0b10000000 | (0b00000000000 | codepoint)
+        print(bin(byte_right))
+        print([byte_left, byte_right])
+        return bytes([byte_left, byte_right])
+
+    elif 2048 <= codepoint <= 4095:
+        byte_left = 0b11100000 | (0b0000000000000000 | codepoint) >> 12
+        print(bin(byte_left))
+        byte_middle = 0b10000000 | (0b0000000000000000 | codepoint) >> 6
+        print(bin(byte_middle))
+        byte_right = 0b10000000 | (0b0000000000000000 | codepoint) << 12
+        print(bin(byte_right))
+        print([byte_left, byte_middle, byte_right])
+        #return bytes([byte_left, byte_middle, byte_right])
 
 
 def decode(bytes_object):
@@ -13,7 +29,10 @@ def decode(bytes_object):
 
 
 def main():
-    print(encode(127))
+    print(encode(163))
+    print()
+    print(encode(2090))
+
 
 if __name__ == "__main__":
     main()
