ae9887e06a0ba1ee0f6978f6fd3b3bcd13fb47c1
osanchez <osanchez@umass.edu>
2018-02-14 02:49:01 -0500

    completed encode

--- main.py
+++ main.py
@@ -2,25 +2,30 @@
 
 def encode(codepoint):
     if codepoint < 128:
+        print([codepoint])
         return bytes([codepoint])
 
     elif 128 <= codepoint <= 2047:
-        byte_left = 0b11000000 | (0b00000000000 | codepoint) >> 6
-        print(bin(byte_left))
-        byte_right = 0b10000000 | (0b00000000000 | codepoint)
-        print(bin(byte_right))
+        byte_left = (0b110 << 5) | (0b11111000000 & codepoint) >> 6
+        byte_right = (0b10 << 6) | (0b00000111111 & codepoint)
         print([byte_left, byte_right])
         return bytes([byte_left, byte_right])
 
-    elif 2048 <= codepoint <= 4095:
-        byte_left = 0b11100000 | (0b0000000000000000 | codepoint) >> 12
-        print(bin(byte_left))
-        byte_middle = 0b10000000 | (0b0000000000000000 | codepoint) >> 6
-        print(bin(byte_middle))
-        byte_right = 0b10000000 | (0b0000000000000000 | codepoint) << 12
-        print(bin(byte_right))
+    elif 2048 <= codepoint <= 65535:
+        byte_left = (0b1110 << 4) | (0b1111000000000000 & codepoint) >> 12
+        byte_middle = (0b10 << 6) | (0b0000111111000000 & codepoint) >> 6
+        byte_right = (0b10 << 6) | (0b0000000000111111 & codepoint)
         print([byte_left, byte_middle, byte_right])
-        #return bytes([byte_left, byte_middle, byte_right])
+        return bytes([byte_left, byte_middle, byte_right])
+
+    elif 65536 <= codepoint <= 1114111:
+        byte_outer_left = (0b11110 << 3) | (0b111000000000000000000 & codepoint) >> 18
+        byte_inner_left = (0b10 << 6) | (0b000111111000000000000 & codepoint) >> 12
+        byte_inner_right = (0b10 << 6) | (0b000000000111111000000 & codepoint) >> 6
+        byte_outer_right = (0b10 << 6) | (0b000000000000000111111 & codepoint)
+        print([byte_outer_left, byte_inner_left, byte_inner_right, byte_outer_right])
+        return bytes([byte_outer_left, byte_inner_left, byte_inner_right, byte_outer_right])
+
 
 
 def decode(bytes_object):
@@ -29,9 +34,13 @@ def decode(bytes_object):
 
 
 def main():
+    print(encode(122))
+    print()
     print(encode(163))
     print()
-    print(encode(2090))
+    print(encode(2048))
+    print()
+    print(encode(65537))
 
 
 if __name__ == "__main__":
