2362e49ffbdb3030701d2fec23cf1a2cc17881eb
Omar S <osanchez@umass.edu>
2018-02-16 19:04:46 -0500

    Completed, but autograder wasnt working to verify

--- strings.py
+++ strings.py
@@ -1,44 +1,56 @@
 import argparse
-import re
-
-import binascii
 
 
 def print_strings(file_obj, encoding, min_len):
-    # Right now all this function does is print its arguments.
-    # You'll need to replace that code with code that actually finds and prints the strings!
-
-    binary_list = []
-
-    if encoding == "s":
-        mode = "UTF-8"
-    elif encoding == "l":
-        mode = "UTF-16-le"
-    elif encoding == "b":
-        mode = "UTF-16-be"
-
-    for block in iter(lambda: file_obj.read(16), b''):
-        raw_byte = binascii.hexlify(block)
-        byte_array = re.sub('(..)', r'\1 ', raw_byte.decode(mode)).split()
-        binary_list.append(byte_array)
-
     string = ""
 
-    # utf-8 length 2
-    for bit_list in binary_list:
-        for bit in bit_list:
-            if 32 <= int(bit, 16) <= 126:
-                string += chr(int(bit, 16))
+    if encoding == 's':
+        byte = file_obj.read(1)
+        while byte:
+            value = int.from_bytes(byte, byteorder='big')
+            if 32 <= value <= 126:
+                string += chr(value)
             else:
                 if len(string) >= min_len:
                     print(string)
                     string = ""
                 else:
                     string = ""
+            byte = file_obj.read(1)
         if len(string) >= min_len:
             print(string)
             string = ""
-        else:
+    elif encoding == 'b':
+        byte = file_obj.read(2)
+        while byte:
+            value = int.from_bytes(byte, byteorder='big')
+            if 32 <= value <= 126:
+                string += chr(value)
+            else:
+                if len(string) >= min_len:
+                    print(string)
+                    string = ""
+                else:
+                    string = ""
+            byte = file_obj.read(2)
+        if len(string) >= min_len:
+            print(string)
+            string = ""
+    elif encoding == 'l':
+        byte = file_obj.read(2)
+        while byte:
+            value = int.from_bytes(byte, byteorder='little')
+            if 32 <= value <= 126:
+                string += chr(value)
+            else:
+                if len(string) >= min_len:
+                    print(string)
+                    string = ""
+                else:
+                    string = ""
+                byte = file_obj.read(2)
+        if len(string) >= min_len:
+            print(string)
             string = ""
 
 
